16c437e9950d5faee0d6c22d2949863e2d17138c
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-01-23 17:02:20 +0100

    add component found [bonus]

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,6 +2,9 @@
 //importazione app single card
 import SingleCard from './SingleCard.vue';
 
+//importazione componente card found
+import TotalFound from './TotalFound.vue';
+
 //importazione store
 import { store } from '../store.js';
 export default{
@@ -11,7 +14,8 @@ export default{
 		};
 	},
 	components:{
-		SingleCard
+		SingleCard,
+		TotalFound
 	},
 	methods:{
 		changeType(value){
@@ -35,7 +39,7 @@ export default{
 		<div class="contaier m-5 bg-white p-5">
 			<div class="row">
 				<div class="col-12 bg-black text-white mb-3">
-					Found {{ store.cards.length }} cards
+					<TotalFound :cardFound="store.cards.length"/>
 				</div>
 					<div v-for="(elem, i ) in store.cards" :key="i" class="col-12 col-sm-6 col-md-2">
 						<SingleCard :card="elem"/>
---
+++ src/components/TotalFound.vue
@@ -0,0 +1,23 @@
+<script>
+export default{
+	data() {
+		return{
+
+		};
+	},
+	props:{
+		cardFound:Number,
+	}
+}
+
+</script>
+
+<template>
+	<div>
+		Found {{ cardFound}} cards
+	</div>
+</template>
+
+<style lang ="scss" scoped>
+
+</style>
